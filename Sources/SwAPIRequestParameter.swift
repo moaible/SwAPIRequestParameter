0228e558e672e455ce8e224f2f391ed47223803f
moaible <motodera@moapp.info>
2017-05-30 01:07:59 +0900

    Add license header comment

--- Sources/SwAPIRequestParameter.swift
+++ Sources/SwAPIRequestParameter.swift
@@ -1,3 +1,26 @@
+/**
+ *  SwAPIRequestParameter
+ *
+ *  Copyright (c) 2017 moaible. Licensed under the MIT license, as follows:
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 
 public protocol APIRequestParameter {
     
@@ -71,7 +94,8 @@ extension APIRequestParameterDictionary: ExpressibleByDictionaryLiteral {
     
     public init(dictionaryLiteral elements: (Key, Value)...) {
         self.init(
-            elements.reduce([Key: Value](minimumCapacity: elements.count)) { (dictionary: [Key: Value], element:(key: Key, value: Value)) -> [Key: Value] in
+            elements.reduce([Key: Value](minimumCapacity: elements.count)) {
+                (dictionary: [Key: Value], element:(key: Key, value: Value)) -> [Key: Value] in
                 var d = dictionary
                 d[element.key] = element.value
                 return d
